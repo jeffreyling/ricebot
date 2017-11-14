d64e56c05fb4f71c879cde56dd358019b961fa63
Jeffrey Ling <jeffrey.r.ling@gmail.com>
2017-11-13 23:10:24 -0800

    .

--- app.py
+++ app.py
@@ -74,10 +74,7 @@ def webhook():
                         message_text = messaging_event["message"]["text"]  # the message's text
                         if message_text == "help":
                             send_message(sender_id,
-                            """Send \"rice 1.5\" to post 1.5 cups of rice.\n
-                                Send \"clear\" to clear your request.\n
-                                Send \"show\" to see who has requested rice so far.\n
-                                You can clear your last request with a new one too.""")
+                            "Send \"rice 1.5\" to post 1.5 cups of rice.\nSend \"clear\" to clear your request.\nSend \"show\" to see who has requested rice so far.\nYou can clear your last request with a new one too.")
                         elif re.match("rice \d+(\.\d+)?", message_text):
                             amt = float(message_text.strip().split()[-1])
                             prev_req = RiceRequest.query.filter_by(name=first_name).first()
